8fe4579b6b8bb2fa1f82938894411140eff8208e
Kevinfunes12 <103433769+Kevinfunes12@users.noreply.github.com>
2022-04-21 12:52:52 -0300

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,9 @@
+let nombre = "kevin"
+let edad = 15;
+console.log (edad)
+prompt ("hola1")
+alert ("hola2")
+let mensaje =prompt ("escriba un mensaje")
+console.log (mensaje);
+nombre =20;
+console.log (nombre)
